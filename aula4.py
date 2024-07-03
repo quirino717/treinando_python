e219eed8e517308fbf443956a0e9556cd7f8bd76
Leonardo Quirino <quirino717@gmail.com>
2024-07-03 17:54:27 -0300

    exercicios

---
+++ aula4.py
@@ -0,0 +1,9 @@
+#nome = input('Qual seu nome? ')
+#idade = input('Qual sua idade? ')
+#peso = input('Qual seu peso? ')
+#print('Bem vindo,', nome, '! Prazer em te conhecer')
+
+num1 = int(input('Qual o primeiro número? '))
+num2 = int(input('Qual o segundo número? '))
+soma = num1 + num2
+print ('O resultado da soma é: ', soma)
